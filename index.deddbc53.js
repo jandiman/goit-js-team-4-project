ffb76d0ddd443550655742c14cab789307fe5834
crux16 <30309887+crux16@users.noreply.github.com>
2024-01-14 17:50:41 +0000

    Deploying to gh-pages from @ jandiman/goit-js-team-4-project@a9369e109911323306b5729da05a07daedbf7062 🚀

--- index.deddbc53.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[footer-modal-open]"),o=document.querySelector("[footer-modal-close]"),t=document.querySelector("[footer-data-modal]");function c(){t.classList.toggle("is-hidden")}e.addEventListener("click",c),o.addEventListener("click",c)}();
-//# sourceMappingURL=index.deddbc53.js.map
